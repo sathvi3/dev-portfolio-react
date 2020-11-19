f5b09cdc3be13de5957615c69f50e36d18f450b2
sathvi3 <sathvikbhandar007@hotmail.com>
2020-11-20 00:29:08 +0530

    added animation to Movie component.

--- src/animation.js
+++ src/animation.js
@@ -110,3 +110,11 @@ export const scrollReveal = {
     },
   },
 };
+
+export const swoopAdoop = {
+  hidden: { width: "0%", transition: { duration: 1 } },
+  show: {
+    width: "100%",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
